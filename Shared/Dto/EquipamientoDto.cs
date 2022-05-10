f5208d3b6e0f7ca86f9d4cb19c21624afd8155b5
djrodriguez <danicuco@hotmail.com>
2022-05-10 13:57:06 +0200

    Project Clean

---
+++ Shared/Dto/EquipamientoDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Reciclaje.Info.Shared.Dto
+{
+    public class EquipamientoDto
+    {
+        public string? Nombre { get; set; }
+
+        public string? Avatar { get; set; }
+        public string? Color { get; set; }
+        public string? Equipamiento { get; set; }
+
+        public string Residuos { get; set; } = "Sin Contenido";
+    }
+}
